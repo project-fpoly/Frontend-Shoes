98c4717cdf0de9a5115eeb86c3611d87f5abf36e
thanh <miktenthanh@gmail.com>
2024-01-22 17:05:43 +0700

    Api config

---
+++ src/core/Api.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost/",
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    console.log("Request Interceptor:", config);
+    return config;
+  },
+  (error) => {
+    console.error("Request Interceptor Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    console.log("Response Interceptor:", response);
+    return response;
+  },
+  (error) => {
+    console.error("Response Interceptor Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
