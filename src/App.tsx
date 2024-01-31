e4cf5139267d37c847b954acd364138f841a66d9
thanh <miktenthanh@gmail.com>
2024-02-01 01:20:46 +0700

    delete owl carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import Router from "./router";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 import "./App.module.scss";
 function App() {
   return (
