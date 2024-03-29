37603fa3bc4d48a49878db9f7797452b2bd41162
Lam Nguyen <ntl03.dev@gmail.com>
2024-03-29 09:55:29 +0700

    fix: check router role

--- src/router/privateRoutes.tsx
+++ src/router/privateRoutes.tsx
@@ -1,15 +1,38 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import {JwtPayload, jwtDecode} from "jwt-decode";
 
 export const PrivateRoute = ({ children, user }) => {
   const navigate = useNavigate()
-
+  const token = localStorage.getItem("accessToken");
   useEffect(() => {
+    const checkToken = () => {
+      if (token === undefined || token === null || token === "") {
+        navigate("/signin");
+      } else {
+        try {
+          const decoded:JwtPayload= jwtDecode(token);
+          const diff = new Date().getTime() / 1000 - decoded!.exp!;
+          if (diff > 0) {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("email");
+            localStorage.removeItem("userID");
+            navigate("/signin");
+          }
+        } catch (error) {
+          console.log("Invalid token: " + token);
+          localStorage.removeItem("accessToken");
+          navigate("/signin");
+        }
+      }
+    };
+
     if (user && user.role !== 'admin') {
       console.error('you is not admin')
       navigate('/')
     }
-  }, [user])
+    checkToken();
+  }, [user,token,navigate])
 
   return user ? children : null
 }
