dc00d5e109a8928590aa77b9f51272d22f2ddd05
nguyentienlam <ntl260303@gmail.com>
2024-04-09 21:43:51 +0700

    fix: create sale

--- src/components/Admin/Sale/FormSale.tsx
+++ src/components/Admin/Sale/FormSale.tsx
@@ -1,156 +1,189 @@
-import React, {useEffect} from "react";
-import { Form, Input, Button, DatePicker } from "antd";
-import { ISale } from "../../../common/sale";
-import dayjs from "dayjs";
+import React, { useEffect } from 'react'
+import { Form, Input, Button, DatePicker, Select } from 'antd'
+import { ISale } from '../../../common/sale'
+import dayjs from 'dayjs'
 
-import {useDispatch, useSelector} from "react-redux";
-import {AppDispatch} from "../../../redux/store.ts";
-import {fetchAllSales} from "../../../features/sale";
-import {IStateSale} from "../../../common/redux/type.ts";
+import { useDispatch, useSelector } from 'react-redux'
+import { AppDispatch, RootState } from '../../../redux/store.ts'
+import { fetchAllSales } from '../../../features/sale'
+import { IStateSale } from '../../../common/redux/type.ts'
+import { fetchAllProducts } from '../../../features/product/index.ts'
+import { IProduct } from '../../../common/products.ts'
 
 type FormSaleProps = {
-  onSubmit: (values: ISale) => void;
-};
-
+  onSubmit: (values: ISale) => void
+}
+const { Option } = Select;
 const FormSale: React.FC<
-    ISale & { onSubmit: (values: ISale) => void; mode: string }
-> = ({name,  description, discount, product, start_date, expiration_date, onSubmit, mode}) => {
-    // console.log(product);
+  ISale & { onSubmit: (values: ISale) => void; mode: string }
+> = ({
+  name,
+  description,
+  discount,
+  product,
+  start_date,
+  expiration_date,
+  onSubmit,
+  mode,
+}) => {
+  // console.log(product);
 
-    const [form] = Form.useForm();
+  const [form] = Form.useForm()
 
   const handleFormSubmitCreate = (values: ISale) => {
     onSubmit(values)
-    console.log(values)
   }
-  const dispatch = useDispatch<AppDispatch>();
+  const {products}=useSelector((state:RootState)=>state.product)
+  const dispatch = useDispatch<AppDispatch>()
   useEffect(() => {
-    dispatch(fetchAllSales({page: 1, limit: 10, keyword: ""}));
-  }, [dispatch]);
-  const {sales} = useSelector(
-      (state: IStateSale) => state.sale
-  );
+    dispatch(fetchAllSales({ page: 1, limit: 10, keyword: '' }))
+    dispatch(fetchAllProducts({ page:1, pageSize:10, searchKeyword:"" }))
+  }, [dispatch])
+  const { sales } = useSelector((state: IStateSale) => state.sale)
 
   const onFinish = (values: any) => {
-    onSubmit(values as ISale);
-  };
+    // onSubmit(values as ISale);
+  }
 
   return (
-      <Form
-          form={form}
-          name="basic"
-          labelCol={{span: 8}}
-          wrapperCol={{span: 16}}
-          autoComplete="off"
-          initialValues={{
-            name,
-            discount,
-            description,
-            start_date: dayjs(start_date),
-            expiration_date: dayjs(expiration_date)
-          }}
-          onFinish={handleFormSubmitCreate}
+    <Form
+      form={form}
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      autoComplete="off"
+      initialValues={{
+        name,
+        discount,
+        description,
+        start_date: dayjs(start_date),
+        expiration_date: dayjs(expiration_date),
+      }}
+      onFinish={handleFormSubmitCreate}
+    >
+      <Form.Item
+        label={'Campaign Name'}
+        name="name"
+        rules={[{ required: true, message: 'Please input Campaign Name' }]}
       >
-        <Form.Item
-            label={"Campaign Name"}
-            name="name"
-            rules={[{required: true, message: "Please input Campaign Name"}]}
-        >
-          <Input/>
-        </Form.Item>
+        <Input />
+      </Form.Item>
 
-        {mode === "create" && (
-            <>
-              <Form.Item
-                  label={"Description"}
-                  name="description"
-                  rules={[{required: true, message: "Please input Description"}]}
-              >
-                <Input.TextArea/>
-              </Form.Item>
+      {mode === 'create' && (
+        <>
+          <Form.Item
+            label={'Description'}
+            name="description"
+            rules={[{ required: true, message: 'Please input Description' }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
 
-              <Form.Item
-                  label={"Discount"}
-                  name="discount"
-                  rules={[{required: true, message: "Please input Discount"}]}
-              >
-                <Input type="number"/>
-              </Form.Item>
+          <Form.Item
+            label={'Discount'}
+            name="discount"
+            rules={[{ required: true, message: 'Please input Discount' }]}
+          >
+            <Input type="number" />
+          </Form.Item>
 
-              <Form.Item
-                  label={"Product"}
-                  name="product"
-                  rules={[{required: true, message: "Please input Product"}]}
-              >
-                <Input type="number"/>
-              </Form.Item>
+          <Form.Item
+      label={'Product'}
+      name="product"
+      rules={[{ required: true, message: 'Please input Product' }]}
+    >
+      <Select
+        mode="multiple"
+        placeholder="Please select products"
+        defaultValue={[]} 
+      >
+        {products.map((product:IProduct) => (
+          <Option key={product._id} value={product._id}>
+            {product.name}
+          </Option>
+        ))}
+      </Select>
+    </Form.Item>
+          <Form.Item
+            label={'Start date'}
+            name="start_date"
+            rules={[{ required: true, message: 'Please input Start date' }]}
+          >
+            <DatePicker />
+          </Form.Item>
+          <Form.Item
+            label={'Expiration date'}
+            name="expiration_date"
+            rules={[
+              { required: true, message: 'Please input Expiration date' },
+            ]}
+          >
+            <DatePicker />
+          </Form.Item>
+        </>
+      )}
+      {mode === 'update' && (
+        <>
+          <Form.Item
+            label={'Description'}
+            name="description"
+            rules={[{ required: true, message: 'Please input Description' }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
 
-              <Form.Item
-                  label={"Start date"}
-                  name="start_date"
-                  rules={[{required: true, message: "Please input Start date"}]}
-              >
-                <DatePicker/>
-              </Form.Item>
-              <Form.Item
-                  label={"Expiration date"}
-                  name="expiration_date"
-                  rules={[{required: true, message: "Please input Expiration date"}]}
-              >
-                <DatePicker/>
-              </Form.Item>
-            </>
-        )}
-        {mode === "update" && (
-            <>
-              <Form.Item
-                  label={"Description"}
-                  name="description"
-                  rules={[{required: true, message: "Please input Description"}]}
-              >
-                <Input.TextArea/>
-              </Form.Item>
+          <Form.Item
+            label={'Discount'}
+            name="discount"
+            rules={[{ required: true, message: 'Please input Discount' }]}
+          >
+            <Input type="number" />
+          </Form.Item>
 
-              <Form.Item
-                  label={"Discount"}
-                  name="discount"
-                  rules={[{required: true, message: "Please input Discount"}]}
-              >
-                <Input type="number"/>
-              </Form.Item>
+          <Form.Item
+      label={'Product'}
+      name="product"
+      rules={[{ required: true, message: 'Please input Product' }]}
+    >
+      <Select
+        mode="multiple"
+        placeholder="Please select products"
+        defaultValue={[]} 
+      >
+        {products.map((product:IProduct) => (
+          <Option key={product._id} value={product._id}>
+            {product.name}
+          </Option>
+        ))}
+      </Select>
+    </Form.Item>
 
-              <Form.Item
-                  label={"Product"}
-                  name="product"
-                  rules={[{required: true, message: "Please input Product"}]}
-              >
-                <Input type="number"/>
-              </Form.Item>
+          <Form.Item
+            label={'Start date'}
+            name="start_date"
+            rules={[{ required: true, message: 'Please input Start date' }]}
+          >
+            <DatePicker />
+          </Form.Item>
+          <Form.Item
+            label={'Expiration date'}
+            name="expiration_date"
+            rules={[
+              { required: true, message: 'Please input Expiration date' },
+            ]}
+          >
+            <DatePicker />
+          </Form.Item>
+        </>
+      )}
 
-              <Form.Item
-                  label={"Start date"}
-                  name="start_date"
-                  rules={[{required: true, message: "Please input Start date"}]}
-              >
-                <DatePicker/>
-              </Form.Item>
-              <Form.Item
-                  label={"Expiration date"}
-                  name="expiration_date"
-                  rules={[{required: true, message: "Please input Expiration date"}]}
-              >
-                <DatePicker/>
-              </Form.Item>
-            </>
-        )}
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Save
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
-        <Form.Item wrapperCol={{offset: 8, span: 16}}>
-          <Button type="primary" htmlType="submit">
-            Save
-          </Button>
-        </Form.Item>
-      </Form>
-  );
-};
-
-export default FormSale;
+export default FormSale
