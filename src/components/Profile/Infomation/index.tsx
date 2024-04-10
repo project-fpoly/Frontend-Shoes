06a196b895ada76ccf2e501c927bc1492e94df03
thanhbc <thanhbc@vnext.vn>
2024-04-10 20:06:56 +0700

    feat: member benefits

--- src/components/Profile/Infomation/index.tsx
+++ src/components/Profile/Infomation/index.tsx
@@ -5,7 +5,7 @@ import moment from 'moment'
 const Infomation = () => {
   const user = useSelector((state: any) => state.auth.user)
   return (
-    <div className="px-16 h-[500px] mt-20 h-auto">
+    <div className="">
       <div className='flex gap-5'>
         <Avatar size={80} src={user?.avt?.url} />
         <div className='flex flex-col gap-3'>
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,16 +1,25 @@
 
 import NavProfile from "../../components/Profile/Nav"
 import Infomation from "../../components/Profile/Infomation"
+import NikeApps from "../../components/Profile/Apps"
+import MemberBenefits from "../../components/Profile/MemberBenefits"
 
 
 const Profile = () => {
-  
+
   return (
     <>
       <div className="flex   justify-center items-center">
         <NavProfile></NavProfile>
       </div>
+      <div className="flex flex-col gap-10 px-16  mt-20">
         <Infomation></Infomation>
+        <MemberBenefits></MemberBenefits>
+        <NikeApps></NikeApps>
+      </div>
+
+
+
     </>
 
   )
