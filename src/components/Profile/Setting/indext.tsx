60753f9f3d651e38a4a0214a83c9f42604bebc23
thanhbc <thanhbc@vnext.vn>
2024-04-20 22:33:16 +0700

    update setting user

--- src/components/Profile/Setting/indext.tsx
+++ src/components/Profile/Setting/indext.tsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react';
 import { ConfigProvider, MenuProps } from 'antd';
 import { FaRegUser } from "react-icons/fa";
 import { Menu } from 'antd';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom'; import { MdOutlineMailOutline } from "react-icons/md";
+import { MdOutlineVerifiedUser } from "react-icons/md";
+
 const Setting = () => {
   const items: MenuProps['items'] = [
     {
       label: <Link to={'/profile/setting'}><p className='text-[20px] '>Account Detail</p></Link>,
       key: 'Account Detail',
       icon: <FaRegUser size='20' />
-      
+    }, {
+      label: <Link to={'/profile/setting/communication-preferences'}><p className='text-[20px] '>Communication Preferences</p></Link>,
+      key: 'Communication',
+      icon: <MdOutlineMailOutline size='20' />
+    }, {
+      label: <Link to={'/profile/settingy/privacy'}><p className='text-[20px] '>Privacy</p></Link>,
+      key: 'Privacy',
+      icon: <MdOutlineVerifiedUser size='20' />
     }
 
   ];
